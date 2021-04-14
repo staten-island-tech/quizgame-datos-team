17ef0d91dcbf18414a96150a3abdda09ea6437a2
robmelly <robert.melnik.5@gmail.com>
2021-04-13 23:43:18 -0400

    FINALLY

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//giving each question a shortcut
 function check() {
   var question1 = document.quiz.question1.value;
   var question2 = document.quiz.question2.value;
@@ -10,7 +11,7 @@ function check() {
   var question9 = document.quiz.question9.value;
   var question10 = document.quiz.question10.value;
   var correct = 0;
-
+  // for every correct question you get 1 more unit added to the value of "correct"
   if (question1 == "a") {
     correct++;
   }
@@ -56,21 +57,25 @@ function check() {
 
   var range;
 
+  //if amount of correct answers is less than 4 you get the bad prompt
   if (correct < 4) {
     range = 2;
   }
-
+  //if amount of correct answers is greater than 3 and less than 8 you get the medium prompt
   if (correct > 3 && correct < 8) {
     range = 1;
   }
-
+  //if amount of correct answers is greater than 7 you get the good prompt
   if (correct > 7) {
     range = 0;
   }
-
+  //makes the previously invisble text now visible
   document.getElementById("after_submit").style.visibility = "visible";
+  //depending on which range you get you get a different prompt displayed
   document.getElementById("message").innerHTML = message[range];
+  //integrates the amount of correct answers into a sentence
   document.getElementById("number_correct").innerHTML =
     "You got " + correct + " correct.";
+  // corrolates an image to the amount of questions you answers correctly
   document.getElementById("images").src = images[range];
 }
