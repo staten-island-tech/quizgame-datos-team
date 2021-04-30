52c18dbc166b784d775ea616faba265dd1f8276e
robmelly <robert.melnik.5@gmail.com>
2021-04-29 23:59:39 -0400

    yo

--- game.js
+++ game.js
@@ -12,12 +12,21 @@ let availableQuestions = [];
 
 let questions = [
   {
+<<<<<<< HEAD
     question: "What is the captial of Russia?",
     choice1: "Moscow",
     choice2: "Saint Petersburg",
     choice3: "Vladivostok",
     choice4: "Sochi",
     answer: 1,
+=======
+    question: "What is 2 + 2?",
+    choice1: "2",
+    choice2: "4",
+    choice3: "21",
+    choice4: "17",
+    answer: 2,
+>>>>>>> parent of dd43a90 (yo)
   },
   {
     question: "The tallest building in the world is located in which city?",
@@ -28,6 +37,7 @@ let questions = [
     answer: 1,
   },
   {
+<<<<<<< HEAD
     question: "Where can you find Machu Picchu?",
     choice1: "Columbia",
     choice2: "Bolivia",
@@ -50,11 +60,33 @@ let questions = [
     choice3: "Monaco",
     choice4: "Vatican City",
     answer: 3,
+=======
+    question:
+      "What percent of American adults believe that chocolate milk comes from brown cows?",
+    choice1: "20%",
+    choice2: "18%",
+    choice3: "7%",
+    choice4: "33%",
+    answer: 3,
+  },
+  {
+    question:
+      "Approximately what percent of U.S. power outages are caused by squirrels?",
+    choice1: "10-20%",
+    choice2: "5-10%",
+    choice3: "15-20%",
+    choice4: "30%-40%",
+    answer: 1,
+>>>>>>> parent of dd43a90 (yo)
   },
 ];
 
 const SCORE_POINTS = 100;
+<<<<<<< HEAD
 const MAX_QUESTIONS = 5;
+=======
+const MAX_QUESTIONS = 4;
+>>>>>>> parent of dd43a90 (yo)
 
 startGame = () => {
   questionCounter = 0;
@@ -66,6 +98,7 @@ startGame = () => {
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
+<<<<<<< HEAD
 
     return window.location.assign("/end.html");
   }
@@ -118,3 +151,100 @@ incrementScore = (num) => {
 };
 
 startGame();
+=======
+
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionsIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
+
+const highScoresList = document.querySelector("#highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
+const username = document.querySelector("#username");
+const saveScoreBtn = document.querySelector("#saveScoreBtn");
+const finalScore = document.querySelector("#finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+  e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+
+  highScores.push(score);
+
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  highScores.splice(5);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
+};
+>>>>>>> parent of dd43a90 (yo)
