dd43a90d2b527ff09702e0dbed0a1da0e2c292e0
robmelly <robert.melnik.5@gmail.com>
2021-04-29 23:41:54 -0400

    yo

--- game.js
+++ game.js
@@ -1,157 +1,113 @@
-const question = document.querySelector("#question");
-const choices = Array.from(document.querySelectorAll(".choice-text"));
-const progressText = document.querySelector("#progressText");
-const scoreText = document.querySelector("#score");
-const progressBarFull = document.querySelector("#progressBarFull");
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
-  {
-    question: "What is 2 + 2?",
-    choice1: "2",
-    choice2: "4",
-    choice3: "21",
-    choice4: "17",
-    answer: 2,
-  },
-  {
-    question: "The tallest building in the world is located in which city?",
-    choice1: "Dubai",
-    choice2: "New York",
-    choice3: "Shanghai",
-    choice4: "None of the above",
-    answer: 1,
-  },
-  {
-    question:
-      "What percent of American adults believe that chocolate milk comes from brown cows?",
-    choice1: "20%",
-    choice2: "18%",
-    choice3: "7%",
-    choice4: "33%",
-    answer: 3,
-  },
-  {
-    question:
-      "Approximately what percent of U.S. power outages are caused by squirrels?",
-    choice1: "10-20%",
-    choice2: "5-10%",
-    choice3: "15-20%",
-    choice4: "30%-40%",
-    answer: 1,
-  },
-];
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 2,
+    },
+    {
+        question:
+            "The tallest building in the world is located in which city?",
+        choice1: "Dubai",
+        choice2: "New York",
+        choice3: "Shanghai",
+        choice4: "None of the above",
+        answer: 1,
+    },
+    {
+        question: "What percent of American adults believe that chocolate milk comes from brown cows?",
+        choice1: "20%",
+        choice2: "18%",
+        choice3: "7%",
+        choice4: "33%",
+        answer: 3,
+    },
+    {
+        question: "Approximately what percent of U.S. power outages are caused by squirrels?",
+        choice1: "10-20%",
+        choice2: "5-10%",
+        choice3: "15-20%",
+        choice4: "30%-40%",
+        answer: 1,
+    }
+]
 
-const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 4;
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
 
 startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  availableQuestions = [...questions];
-  getNewQuestion();
-};
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
 
 getNewQuestion = () => {
-  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
-    return window.location.assign("/end.html");
-  }
-
-  questionCounter++;
-  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
-  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionsIndex];
-  question.innerText = currentQuestion.question;
-
-  choices.forEach((choice) => {
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
-  });
-
-  availableQuestions.splice(questionsIndex, 1);
-
-  acceptingAnswers = true;
-};
-
-choices.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
-    if (!acceptingAnswers) return;
-
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
-
-    let classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-    if (classToApply === "correct") {
-      incrementScore(SCORE_POINTS);
+        return window.location.assign('/end.html')
     }
 
-    selectedChoice.parentElement.classList.add(classToApply);
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 1000);
-  });
-});
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
 
-incrementScore = (num) => {
-  score += num;
-  scoreText.innerText = score;
-};
+    availableQuestions.splice(questionsIndex, 1)
 
-startGame();
+    acceptingAnswers = true
+}
 
-const highScoresList = document.querySelector("#highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
 
-highScoresList.innerHTML = highScores
-  .map((score) => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
-const username = document.querySelector("#username");
-const saveScoreBtn = document.querySelector("#saveScoreBtn");
-const finalScore = document.querySelector("#finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
-const MAX_HIGH_SCORES = 5;
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
 
-finalScore.innerText = mostRecentScore;
+        selectedChoice.parentElement.classList.add(classToApply)
 
-username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.value;
-});
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
 
-saveHighScore = (e) => {
-  e.preventDefault();
+        }, 1000)
+    })
+})
 
-  const score = {
-    score: mostRecentScore,
-    name: username.value,
-  };
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
 
-  highScores.push(score);
-
-  highScores.sort((a, b) => {
-    return b.score - a.score;
-  });
-
-  highScores.splice(5);
-
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
-};
+startGame()
